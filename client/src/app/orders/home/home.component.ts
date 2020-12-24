c988941d9d770f765707d9b7719d23f980ffc2c7
mandy94 <majasavic199@gmail.com>
2020-12-24 21:56:54 +0100

    3.24 orders module init

---
+++ client/src/app/orders/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'orders-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class OrdersHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ client/src/app/orders/orders.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { OrdersHomeComponent } from './home/home.component';
+
+export const ordersRoutes: Routes = [
+    {
+        path: '',
+        component: OrdersHomeComponent,
+    }
+];
